cc68a9bfea6270fbc5aadd5021caa27fab130369
Nyanchl <rotdicm@gmail.com>
2024-07-23 10:27:25 +0900

    spa test

--- src/Myapps.tsx
+++ src/Myapps.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Rhythm from "./myapps/rhythmgame";
 
 export const Myapps: React.VFC = () => {
   return (
-    <Switch>
+    <Routes>
       <Route path="/rhythm">
         <Rhythm />
       </Route>
       <h1>Hello, world!</h1>
       <h2>Start editing to see some magic happen!</h2>
-    </Switch>
+    </Routes>
   )
 }
 
