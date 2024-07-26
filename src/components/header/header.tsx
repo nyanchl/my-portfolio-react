b944781d9b4c1e41ad5a652ef5913109208fc4d7
Nyanchl <rotdicm@gmail.com>
2024-07-26 09:45:47 +0900

    Header追加

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,15 @@
 import { HeaderText } from "../atomics/header_text";
 
-export const Header = () => {
-    <>
+const Header = () => {
+    return (
+        <>
         <div>
             <HeaderText link="/" color="#5C5959" text="About" />
             <HeaderText link="/about" color="#5C5959" text="Skill" />
             <HeaderText link="/contact" color="#5C5959" text="Works" />
         </div>
     </>
+    )
 }
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Myapps from './Myapps.tsx'
+import Header from './components/header/header.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Header />
     <h1>Nyanchl's Portfolio</h1>
     <Myapps />
   </React.StrictMode>
